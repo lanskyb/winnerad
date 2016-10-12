85c8de7a4960baae2c9cfe800b7cea3ba82e76ce
blanskygit <blansky@wellgo.net>
2016-10-12 16:41:37 +0300

    Added worker support in storage verticle, the version of the vert.x was upgraded to 3.3.3 (from: 3.0.0)

--- winnerad-content-provider/src/main/java/com/wellgo/wad/contentprovider/contentgenerator/ContentGeneratorVerticle.java
+++ winnerad-content-provider/src/main/java/com/wellgo/wad/contentprovider/contentgenerator/ContentGeneratorVerticle.java
@@ -45,34 +45,35 @@ public class ContentGeneratorVerticle extends AbstractVerticle {
 		     logger.debug("ContentGeneratorVerticle - I have received a message: " + message.body());
 		     
 		     
-		     vertx.eventBus().send(Configuration.EB_ADDR_CONTENT_STORAGE, message.body(), ar -> {
-           	  if (ar.succeeded()) {
+		     vertx.eventBus().send(Configuration.EB_ADDR_CONTENT_STORAGE, message.body(), 
+		    		 ar -> {
+		    			 		if (ar.succeeded()) {
            		
-           		  logger.debug("ContentGeneratorVerticle - received from storage data: " + ar.result().body());
-           		  
-           		  
-           		  String resultJsTemplate = "<a href=\"javascript:\" onclick=\"window.open('%s');\" target=\"_blank\">Sample Code id=%s</a>";
-           		  String resultJs="";
-           		  
-           		  JSONArray jWizards = new JSONArray(ar.result().body().toString());
-	    		  
-	    		  for (int i = 0, size = jWizards.length(); i < size; i++) {
-	    		      JSONObject objectInArray = jWizards.getJSONObject(i);
-	    		      logger.debug("ContentGeneratorVerticle - " + objectInArray.get("id") + "<--->" + objectInArray.get("url"));
-	    		      resultJs += String.format(resultJsTemplate, objectInArray.get("url"), objectInArray.get("id"));
-	    		    }
-           		        
-           		    
-           		// Generating content
-	    		  message.reply( ar.result().body());
-       		     
-      		    // message.reply(message.body());
-           	  }
-           	  else {
-           		logger.error("Failed to get data from storage");
-           		message.reply("{\"error\": \"Failed to get data from storage\"}");
-           	  }
-           });
+					           		  logger.debug("ContentGeneratorVerticle - received from storage data: " + ar.result().body());
+					           		  
+					           		  
+					           		  String resultJsTemplate = "<a href=\"javascript:\" onclick=\"window.open('%s');\" target=\"_blank\">Sample Code id=%s</a>";
+					           		  String resultJs="";
+					           		  
+					           		  JSONArray jWizards = new JSONArray(ar.result().body().toString());
+						    		  
+						    		  for (int i = 0, size = jWizards.length(); i < size; i++) {
+						    		      JSONObject objectInArray = jWizards.getJSONObject(i);
+						    		      logger.debug("ContentGeneratorVerticle - " + objectInArray.get("id") + "<--->" + objectInArray.get("url"));
+						    		      resultJs += String.format(resultJsTemplate, objectInArray.get("url"), objectInArray.get("id"));
+						    		    }
+					           		        
+					           		    
+					           		// Generating content
+						    		message.reply( ar.result().body());
+					       		     
+					      		    // message.reply(message.body());
+					           	  }
+					           	  else {
+					           		logger.error("Failed to get data from storage");
+					           		message.reply("{\"error\": \"Failed to get data from storage\"}");
+					           	  }
+		    		 });
 		     
 		     
 		      // Generating content
--- winnerad-content-provider/src/main/java/com/wellgo/wad/contentprovider/contentstorage/ContentStorageVerticle.java
+++ winnerad-content-provider/src/main/java/com/wellgo/wad/contentprovider/contentstorage/ContentStorageVerticle.java
@@ -13,8 +13,8 @@ import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import io.vertx.core.Context;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
+
 import com.wellgo.wad.contentprovider.Configuration;
 
 
@@ -28,7 +28,22 @@ import io.vertx.core.logging.LoggerFactory;
 
 public class ContentStorageVerticle extends AbstractVerticle {
 
-	private TransportClient client;
+	private TransportClient esClient;
+	
+	private String esClusterName;
+	
+	private JsonArray esNodes;
+	
+	private WorkerExecutor executor;
+	
+	 // default value - 10 threads max 
+    private int exPoolSize = 10;
+
+    // default value - 20 seconds
+    private long maxExecuteTime = 20000;
+    
+    private final static String  EX_POOL_NAME = "content-storage-verticle-ex-pool";
+	
 	
 	private final static Logger logger = LoggerFactory.getLogger(ContentStorageVerticle.class);
 	
@@ -41,43 +56,53 @@ public class ContentStorageVerticle extends AbstractVerticle {
 		   consumer.handler(message -> {
 		     logger.info("ContentStorageVerticle - I have received a message: " + message.body());
 		     
+		  
 		     
-		     // Retrieve the content
-		     GetResponse response = client.prepareGet("publisher", "wizard", "publisher123").get();
-		     
-		    
-		     
-		     //String widgets = (String) response.getField("widgets").getValue();
-		     //logger.debug("ContentGeneratorVerticle - Retrived from the storage widgetUrl" + widgets);
-		     
-		     if (!response.isExists()){ // || response.getField(PATH_FIELD) == null) {
-		         // doc not found
-		    	 logger.error("ContentStorageVerticle - Document not found");
-		    	 message.reply("{\"error\": \"Document not found\"}");
-		     }
-		     else {
-		    	 // return response.getField(PATH_FIELD).getValue().toString();
+		     executor.executeBlocking(future -> {
 		    	 
-		    	 logger.debug("ContentStorageVerticle - set size: " + response.getSource().size());
-		    	 
-		    	 if (!response.getSource().isEmpty()) {
+		    	// Retrieve the content
+		    	 GetResponse response = esClient.prepareGet("publisher", "wizard", "publisher123").get();
+			     
+		         future.complete(response); // complete(result);
+		         
+		       }, res -> {
+		    	   
+		    	   //String widgets = (String) response.getField("widgets").getValue();
+				     //logger.debug("ContentGeneratorVerticle - Retrieved from the storage widgetUrl" + widgets);
+		    	   
+		    	   GetResponse response = (GetResponse) res.result();
+				     
+				     if (response == null || !response.isExists()){ // || response.getField(PATH_FIELD) == null) {
+				         // doc not found
+				    	 logger.error("ContentStorageVerticle - Document not found");
+				    	 message.reply("{\"error\": \"Document not found\"}");
+				     }
+				     else {
+				    	 // return response.getField(PATH_FIELD).getValue().toString();
+				    	 
+				    	 logger.debug("ContentStorageVerticle - set size: " + response.getSource().size());
+				    	 
+				    	 if (!response.getSource().isEmpty()) {
 
-		    		 JSONObject json = new JSONObject(response.getSourceAsString());
-		    		 
-		    		  JSONArray jWizards = json.getJSONArray("wizards");
-		    		  
-		    		  for (int i = 0, size = jWizards.length(); i < size; i++) {
-		    		      JSONObject objectInArray = jWizards.getJSONObject(i);
-		    		      logger.debug(objectInArray.get("id") + "<--->" + objectInArray.get("url"));
-		    		    }
-		    		  
-		    		  message.reply(jWizards.toString());
-			     }
-		    	 else {
-		    		 logger.error("ContentStorageVerticle - Empty response received");
-			    	 message.reply("{\"error\": \"Empty response received\"}");
-		    	 }
-		     }
+				    		 JSONObject json = new JSONObject(response.getSourceAsString());
+				    		 
+				    		  JSONArray jWizards = json.getJSONArray("wizards");
+				    		  
+				    		  for (int i = 0, size = jWizards.length(); i < size; i++) {
+				    		      JSONObject objectInArray = jWizards.getJSONObject(i);
+				    		      logger.debug(objectInArray.get("id") + "<--->" + objectInArray.get("url"));
+				    		    }
+				    		  
+				    		  message.reply(jWizards.toString());
+					     }
+				    	 else {
+				    		 logger.error("ContentStorageVerticle - Empty response received");
+					    	 message.reply("{\"error\": \"Empty response received\"}");
+				    	 }
+				     }
+		       });
+
+		     		    
 		     
 		     /*
     		 for (Map.Entry<String, Object> entry : response.getSource().entrySet())
@@ -92,32 +117,10 @@ public class ContentStorageVerticle extends AbstractVerticle {
 	private void initEsClient() {
 		
 		logger.debug("initEsClient >>");
-		
-		String esClusterName = "esonaws";
-		
-		
-		JsonArray esNodes = null;
-		
-		JsonObject config = this.getVertx().getOrCreateContext().config();
-		
-		if (config != null) {
-		
-			esClusterName = config.getString("es.cluster.name", "wad-cp-es");
-		
-		
-			logger.info("Received configuration: es.cluster.name: <" + esClusterName + "> ");
-			
-			esNodes = config.getJsonArray("es.cluster.nodes");
-			
-		}
-		else {
-			logger.warn("Received configuration is empty, using default values");
-		}
-		
+	
 		
 		Settings settings = Settings.settingsBuilder()
 		        .put("cluster.name", esClusterName).build();
-		
 		
 		
 		if (esNodes == null) {
@@ -128,32 +131,25 @@ public class ContentStorageVerticle extends AbstractVerticle {
 		
 		logger.info("Received configuration: es.cluster.nodes: <" + esNodes.encode() + "> ");
 		
-		client = TransportClient.builder().settings(settings).build();
+		esClient = TransportClient.builder().settings(settings).build();
 		
-		for (int i = 0; i < esNodes.size(); i++) {
-		
-			try {
-				client
-				.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(esNodes.getString(i)), 9300))
-				.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(esNodes.getString(i), 9301)));
-				logger.info("Added ES transport for host: " + esNodes.getString(i));
-			} catch (UnknownHostException e) {
-				logger.error("ContentStorageVerticle - initEsClient failed :" + e.getMessage(), e);
-			}
-		}
-
-		
-		
-		/*
-		try {
-			client = TransportClient.builder().settings(settings).build()
-				        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("127.0.0.1"), 9300))
-				        .addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9301)));
-		} catch (UnknownHostException e) {
-			logger.error("ContentStorageVerticle - initEsClient failed :" + e.getMessage(), e);
-		}
-		*/
-		
+		executor.executeBlocking(future -> {
+			
+			for (int i = 0; i < esNodes.size(); i++) {
+				
+				try {
+					esClient
+					.addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(esNodes.getString(i)), 9300))
+					.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress(esNodes.getString(i), 9301)));
+					logger.info("Added ES transport for host: " + esNodes.getString(i));
+				} catch (UnknownHostException e) {
+					logger.error("ContentStorageVerticle - initEsClient failed :" + e.getMessage(), e);
+				}
+			  }
+				future.complete(esNodes); 
+		    }, res -> {
+		       logger.info("ContentStorageVerticle - initEsClient completed, the initialized nodes: " + res.result());
+		    });
 		
 		logger.info("ContentStorageVerticle - initEsClient <<");
 	}
@@ -163,16 +159,38 @@ public class ContentStorageVerticle extends AbstractVerticle {
 	    public void stop(Future<Void> stopFuture) throws Exception {
 			// on shutdown
 		  logger.info("ContentStorageVerticle - stop >>>");
-			client.close();
+		  esClient.close();
 		  logger.info("ContentStorageVerticle - stop <<<");
 	    }
 	  
 	  
 	  @Override
 	  public void init(Vertx vertx, Context context) {
+		  
 		  this.vertx = vertx;
-		  initEsClient();
-	  }
+		  
+		  JsonObject config = this.getVertx().getOrCreateContext().config();
+			
+			if (config != null) {
+			
+				esClusterName = config.getString("es.cluster.name", "wad-cp-es");
+			
+			
+				logger.info("Received configuration: es.cluster.name: <" + esClusterName + "> ");
+				
+				esNodes = config.getJsonArray("es.cluster.nodes");
+				
+			}
+			else {
+				logger.warn("Received configuration is empty, using default values");
+			}
+			
+		 
+			executor = this.vertx.createSharedWorkerExecutor(EX_POOL_NAME, exPoolSize, maxExecuteTime);
+			  
+
+			initEsClient();
+	    }
 
 	
 	
